e4de2ccf863709ba5d90368e7a92edd6c751a137
Michael Friend <mrf7@vt.edu>
2019-10-17 21:29:18 -0400

    Kotlin everything bb and switch to android x and add ktx

--- app/src/main/java/com/mfriend/djapp/TestingStuff.kt
+++ app/src/main/java/com/mfriend/djapp/TestingStuff.kt
@@ -2,7 +2,7 @@ package com.mfriend.djapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import com.spotify.sdk.android.authentication.AuthenticationClient
