a038b739c3155bedb6b56f4f3e738386e8904855
Michael Friend <mfriend@lutron.com>
2020-02-16 16:02:26 -0500

    Implemented add song screen

--- app/src/main/java/com/mfriend/djapp/KoinModules.kt
+++ app/src/main/java/com/mfriend/djapp/KoinModules.kt
@@ -5,6 +5,7 @@
  */
 package com.mfriend.djapp
 
+import com.mfriend.djapp.spotifyapi.models.Playlist
 import com.mfriend.djapp.tempUi.ApiViewModel
 import com.mfriend.djapp.tempUi.PlaylistViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -13,4 +14,5 @@ import org.koin.dsl.module
 val appModule = module {
     viewModel { ApiViewModel(get()) }
     viewModel { PlaylistViewModel(get()) }
+    viewModel { (playlist: Playlist) -> AddSongViewModel(get(), playlist) }
 }
