3be677ab73ffe614d3d3a81f51f9aa6fe6c4a542
mrf7 <mrf7@vt.edu>
2020-05-07 16:23:15 -0400

    Feature/16 30 cleanup api (#41)
    * first pass at handling a paging object
    
    * Switched to a more functional pattern in review requests screen
    
    * Added the extendo comment
    
    * Switched to join to string
    
    * ADded comments and switched to use Either method
    
    * Switched repo to use DB for requests
    
    * Back to Either bb
    
    * Fix detekt issues
    
    * Fix detekt issues
    
    * Removed todo
    
    * fixed detekt warning
    
    * Feature/add timber (#40)
    
    * added timber
    
    * Added timber
    
    Co-authored-by: Michael Friend <mfriend@lutron.com>

--- app/src/main/java/com/mfriend/djapp/common/DJApplication.kt
+++ app/src/main/java/com/mfriend/djapp/common/DJApplication.kt
@@ -6,15 +6,18 @@
 package com.mfriend.djapp.common
 
 import android.app.Application
+import com.mfriend.djapp.BuildConfig
 import com.mfriend.djapp.common.servicelocator.appModule
 import com.mfriend.djapp.spotifyapi.SpotifyModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class DJApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        setupTimber()
         startKoin {
             androidContext(this@DJApplication)
             androidLogger()
@@ -25,6 +28,15 @@ class DJApplication : Application() {
                     appModule
                 )
             )
+        }
+    }
+
+    /**
+     * Plant some trees for the one true god
+     */
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
