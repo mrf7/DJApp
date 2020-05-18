79a678ed0ba0945e412bf594ab1c1a1e9e8cfd84
Michael Friend <mfriend@lutron.com>
2020-05-17 23:54:54 -0400

    added ktlint

--- app/src/main/java/com/mfriend/djapp/spotifyapi/models/SpotifyErrorBody.kt
+++ app/src/main/java/com/mfriend/djapp/spotifyapi/models/SpotifyErrorBody.kt
@@ -2,9 +2,8 @@ package com.mfriend.djapp.spotifyapi.models
 
 import com.squareup.moshi.Json
 
-// Error objects defined by https://developer.spotify.com/documentation/web-api/
 /**
- * Generic API error response body
+ * Generic API error response body defined by  https://developer.spotify.com/documentation/web-api/
  *
  * @property status an HTTP status code defined by https://developer.spotify.com/documentation/web-api/#response-status-codes
  * @property message short description of the error cause
