e2362300b42b576e098469c2faf8d34680446313
Michael Friend <mfriend@lutron.com>
2020-05-17 23:34:16 -0400

    Removed auto dependency thing. Ned to reorganize Libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 buildscript {
 
 
@@ -12,12 +14,34 @@ buildscript {
     }
 }
 plugins {
-    id("io.gitlab.arturbosch.detekt") version "1.7.1"
-    buildSrcVersions
+    id("io.gitlab.arturbosch.detekt") version "1.9.1"
+    id("com.github.ben-manes.versions") version "0.28.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
 
-allprojects {
-    apply(file("$rootDir/detekt.gradle"))
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
+
+    ktlint {
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    detekt {
+        config = files("$rootDir/config/detekt/detekt.yml")
+        parallel = true
+        reports {
+            html {
+                enabled = true
+                destination = file("build/reports/detekt.html")
+            }
+        }
+    }
 }
 
 allprojects {
@@ -28,8 +52,14 @@ allprojects {
     }
 }
 
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
+
+fun isNonStable(version: String) = "^[0-9,.v-]+(-r)?$".toRegex().matches(version).not()
