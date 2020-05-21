e6e32d578c5828dac0f12b1f467009df5b84aaad
Michael <mrf7@vt.edu>
2020-05-20 22:24:28 -0400

    More comments

--- app/src/main/java/com/mfriend/djapp/spotifyapi/models/Image.kt
+++ app/src/main/java/com/mfriend/djapp/spotifyapi/models/Image.kt
@@ -3,12 +3,19 @@ package com.mfriend.djapp.spotifyapi.models
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * Response data holder for the spotify api's representation of an image
+ *
+ * @property url to fetch the image from
+ * @property height of the image in pixels or null if unknown
+ * @property width of the image in pixels or null if unknown
+ */
 @JsonClass(generateAdapter = true)
 data class Image(
     @Json(name = "height")
-    val height: Int,
+    val height: Int?,
     @Json(name = "url")
     val url: String,
     @Json(name = "width")
-    val width: Int
+    val width: Int?
 )
